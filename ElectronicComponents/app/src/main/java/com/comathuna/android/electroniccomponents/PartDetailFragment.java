733e2f362908109c582fc061cef923dac46ffe59
Conor O'Mathuna <b00075707@student.itb.ie>
2016-11-08 11:17:55 +0000

    Commit USB changes

--- ElectronicComponents/app/src/main/java/com/comathuna/android/electroniccomponents/PartDetailFragment.java
+++ ElectronicComponents/app/src/main/java/com/comathuna/android/electroniccomponents/PartDetailFragment.java
@@ -1,41 +1,65 @@
 package com.comathuna.android.electroniccomponents;
 
-import android.app.Fragment;
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PartDetailFragment extends Fragment {
 
-    private long partId;
+    private static final String PART_ID_KEY_INDEX = "part_id_index";
+    private static final boolean LOCAL_LOGD = true;
+    private static final String TAG = "PartDetailFragment";
+    private long mPartId;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (LOCAL_LOGD) Log.d(TAG, "onCreate() called");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            mPartId = savedInstanceState.getLong(PART_ID_KEY_INDEX);
+        }
+        if (LOCAL_LOGD) Log.d(TAG, "onCreateView() called");
         return inflater.inflate(R.layout.fragment_part_detail, container, false);
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        if (LOCAL_LOGD) Log.d(TAG, "onStart() called");
         View view= getView();
         if (view != null) {
+            Part part = Part.sParts[(int) mPartId];
+
             TextView title = (TextView) view.findViewById(R.id.textTitle);
-            Part part = Part.sParts[(int) partId];
             title.setText(part.getName());
+
             TextView description = (TextView) view.findViewById(R.id.textDescription);
             description.setText(part.getDescription());
+
+            ImageView image = (ImageView) view.findViewById(R.id.imagePartIcon);
+            image.setImageResource(part.getImageResourceId());
         }
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong(PART_ID_KEY_INDEX, mPartId);
+        if (LOCAL_LOGD) Log.d(TAG, "onSaveInstanceState() called");
+    }
+
     public void setPart(long id){
-        this.partId = id;
+        this.mPartId = id;
+        if (LOCAL_LOGD) Log.d(TAG, "setPart() called");
     }
 }
